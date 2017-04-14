c477c9f810502b1acd2d756678d92425e58f1c8a
김승찬 <ksc5348@naver.com>
2017-04-14 14:12:59 +0900

    world 추가

--- 20106/src/Test1.java
+++ 20106/src/Test1.java
@@ -4,6 +4,12 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		System.out.println("world");
+		
+		
+		
+		
+		
 	}
 
 }
